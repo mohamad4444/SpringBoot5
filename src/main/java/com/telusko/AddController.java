31509a621692624479e8b9bd506ad6695692728b
Mohammad Mahamid <mzkariyah@gmail.com>
2023-03-24 14:03:44 +0100

    @RequestParam

--- src/main/java/com/telusko/AddController.java
+++ src/main/java/com/telusko/AddController.java
@@ -5,13 +5,14 @@ import javax.servlet.http.HttpServletResponse;
 import com.telusko.service.AddService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class AddController {
 	@RequestMapping("/add")
-	public ModelAndView add(HttpServletRequest request,HttpServletResponse response) {
-		int i=Integer.parseInt(request.getParameter("t1"));
-		int j=Integer.parseInt(request.getParameter("t2"));
+	public ModelAndView add(@RequestParam("t1") int i,@RequestParam("t1") int j,HttpServletRequest request,HttpServletResponse response) {
+//		int i=Integer.parseInt(request.getParameter("t1"));
+//		int j=Integer.parseInt(request.getParameter("t2"));
 		//int k=i+j;
 		AddService as =new AddService();
 		int k=as.add(i, j);
