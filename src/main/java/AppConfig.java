c57a026a547ffd562735c0507913ce861dc95e2a
Mohammad Mahamid <mzkariyah@gmail.com>
2023-03-29 19:31:43 +0200

    WebInitilizer

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,20 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.example.controller" })
+public class AppConfig {
+ 
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
